06623b3b33378f5d4361ca26fb6ce498c0d3b200
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2019-08-09 13:07:38 +0200

    Innit Commit

---
+++ src/main/java/no/tdd/tddapp/Main.java
@@ -0,0 +1,8 @@
+package no.tdd.tddapp;
+
+public class Main {
+
+    public static void main(String... args){
+        System.out.println("Let's test!");
+    }
+}
---
+++ src/test/java/no/tdd/tddapp/MainTest.java
@@ -0,0 +1,18 @@
+package no.tdd.tddapp;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void testMain(){
+        System.out.println("This is a bad test!");
+    }
+}
